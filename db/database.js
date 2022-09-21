e6200c05fae55d15a3744fbecbc155d1e0b77f8d
metrgram118 <ihctuyd@mail.ru>
2022-09-21 15:24:59 +0300

    fix http request

--- db/database.js
+++ db/database.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
+  develloped: {
+    username: "intro_user",
+    password: "intro_user",
+    
+  },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
